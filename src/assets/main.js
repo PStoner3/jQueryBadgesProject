252ff86b6d4a1626c42c67d5590aabbeefced2f1
Paul Stoner <pstoner6765@gmail.com>
2017-01-17 12:41:05 -0500

    Changes for project

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,5 +1,18 @@
-$(function() {
+$(function () {
 
-  // your code will go here
-
+  $(document).ready(function () {
+    $.ajax({
+      url: 'https://codeschool.pluralsight.com/users/2033027.json',
+      dataType: 'jsonp',
+      success: function (response) {
+        $(response.courses.completed).each(function (i, course) {
+          $("#badges").append(
+            "<div class='course'>" +
+            "<h3>" + course.title + "</h3>" +
+            "<img src='" + course.badge + "'/>" +
+            "<a href='" + course.url + "' target='_blank' class='btn btn-primary'>See Course</a></div>")
+        })
+      }
+    });
+  });
 });
